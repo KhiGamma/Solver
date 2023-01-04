c0c1de1097fa82e52b1eb3947be4b3d895a35634
ArnaudJuilien <arnaud.azrod@gmail.com>
2023-01-04 11:54:32 +0100

    update test

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,6 +5,7 @@ import obj.NReines;
 
 public class Main {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         /*CSP monCSP = new CSP(4, 4);
         monCSP.genererCSP(0.2, 0.8);
         monCSP.afficherCSP();
@@ -25,5 +26,68 @@ public class Main {
         nReines.solverBJ();
         end = System.nanoTime();
         System.out.println("BJ: " + ((end - start) / Math.pow(10, 9)) + "sec");
+=======
+        /*
+        CSP monCSP = new CSP(100, 10);
+        monCSP.genererCSP(1, 1);
+        //monCSP.afficherCSP();
+
+
+        long start = System.nanoTime();
+        monCSP.solverBT();
+        long end = System.nanoTime();
+        System.out.println("BT: " + ((end - start) / Math.pow(10, 9)) + "sec");
+
+
+        start = System.nanoTime();
+        monCSP.solverBJ();
+        end = System.nanoTime();
+        System.out.println("BJ: " + ((end - start) / Math.pow(10, 9)) + "sec");
+        */
+
+        /*
+        start = System.nanoTime();
+        monCSP.solverFC();
+        end = System.nanoTime();
+        System.out.println("FC: " + ((end - start) / Math.pow(10, 9)) + "sec");
+         */
+
+
+        /*
+        CSP monCSP = new CSP(4, 4);
+        // TODO pb densité != 1
+        monCSP.genererCSP(0.2, 0.9);
+        monCSP.afficherCSP();
+        monCSP.solverFC();
+        */
+
+
+
+        ////////////////////////
+        //NREINES
+        ///////////////////////
+
+        NReines nReines = new NReines(24);
+        nReines.genererCSP();
+        //nReines.afficherCSP();
+
+
+        long start = System.nanoTime();
+        nReines.solverBT();
+        long end = System.nanoTime();
+        System.out.println("BT: " + ((end - start) / Math.pow(10, 9)) + "sec");
+
+
+        start = System.nanoTime();
+        nReines.solverBJ();
+        end = System.nanoTime();
+        System.out.println("BJ: " + ((end - start) / Math.pow(10, 9)) + "sec");
+
+
+        start = System.nanoTime();
+        nReines.solverFC();
+        end = System.nanoTime();
+        System.out.println("FC: " + ((end - start) / Math.pow(10, 9)) + "sec");
+>>>>>>> Stashed changes
     }
 }
